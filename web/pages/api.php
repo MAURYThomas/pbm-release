298b0accd632949081383315860fb8b295d9b728
Thomas MAURY <thomas.maury@pbm.loc>
2018-11-28 16:52:25 +0100

    end of the day push

---
+++ web/pages/api.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>API SOAP</title>
+    </head>
+
+    <body>
+        <h1>TP API SOAP</h1>
+        <?php <form method="post" action="cible.php">?>
+        <p>
+            login
+        </p>
+        <?php </form> ?>
+    </body>
+</html>
